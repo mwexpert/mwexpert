9ce4d93b56e6c883b62295f838285cda6ba50e3f
MW EXPERT <Mylaneerf@gmail.com>
2025-04-22 19:34:06 +0300

    Update wsgi.py

--- mwsolutions/wsgi.py
+++ mwsolutions/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for mwsolutions project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mwsolutions.settings')
 
+# Create the WSGI application callable
 application = get_wsgi_application()
